0c2bb013703806c24855a48064df34612fae6cc1
olbender <olbender@users.noreply.github.com>
2018-06-14 03:45:12 +0200

    Added detection of other Kiwis and lane markings.

--- src/opendlv-perception-vor.cpp
+++ src/opendlv-perception-vor.cpp
@@ -30,6 +30,8 @@
 #include "cluon-complete.hpp"
 #include "opendlv-standard-message-set.hpp"
 
+#include "detect-kiwi.hpp"
+
 int32_t main(int32_t argc, char **argv) {
   int32_t retCode{0};
   auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
@@ -40,12 +42,16 @@ int32_t main(int32_t argc, char **argv) {
     std::cerr << "         --cropbottom: value for cropping image from bottom" << std::endl;
     std::cerr << "         --width:      the width of the image inside the shared memory" << std::endl;
     std::cerr << "         --height:     the height of the image inside the shared memory" << std::endl;
+    std::cerr << "         --widthscaled: the width of the image used in image processing" << std::endl;
+    std::cerr << "         --heightscaled: the height of the image used in image processing" << std::endl;
     std::cerr << "         --bpp:        the bits per pixel of the image inside the shared memory" << std::endl;
     std::cerr << "         --haarfile:   name of the file containing the haar classifier" << std::endl;
     std::cerr << "         --shmin:      name of the shared memory to read from (as consumer) the input image" << std::endl;
     std::cerr << "         --shmout:     name of the shared memory to write to (as producer) the output image" << std::endl;
+    std::cerr << "         --widthout:   the width of the image inside the shared output memory" << std::endl;
+    std::cerr << "         --heightout:  the height of the image inside the shared output memory" << std::endl;
     std::cerr << "         --hranges:    list of H-color ranges used for the image processing: min0:max0,min1:max1,min2:max2,..." << std::endl;
-    std::cerr << "Example: " << argv[0] << " --cid=111 --width=1024 --height=768 --bpp=24 --shmin=cam0 --shmout=output --hranges=1:2,3:4 --haarfile=myHaarFile.xml --croptop=10 --cropbottom=20 --verbose" << std::endl;
+    std::cerr << "Example: " << argv[0] << " --cid=111 --width=1280 --height=960 --bpp=24 --widthout=128 --heightout=96 --shmin=cam0 --shmout=output --hranges=1:2,3:4 --haarfile=myHaarFile.xml --croptop=10 --cropbottom=20 --verbose" << std::endl;
     retCode = 1;
   } else {
     bool const VERBOSE{commandlineArguments.count("verbose") != 0};
@@ -58,6 +64,13 @@ int32_t main(int32_t argc, char **argv) {
     std::string const HAARFILE{commandlineArguments["haarfile"]};
     std::string const SHMIN_NAME{(commandlineArguments["shmin"].size() > 0) ? commandlineArguments["shmin"] : "/cam0"};
     std::string const SHMOUT_NAME{(commandlineArguments["shmout"].size() > 0) ? commandlineArguments["shmout"] : "/output"};
+
+    uint32_t const WIDTH_SCALED{(commandlineArguments["widthscaled"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["widthscaled"])) : 128};
+    uint32_t const HEIGHT_SCALED{(commandlineArguments["heightscaled"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["heightscaled"])) : 96};
+    
+    uint32_t const WIDTH_OUT{(commandlineArguments["widthout"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["widthout"])) : 640};
+    uint32_t const HEIGHT_OUT{(commandlineArguments["heightout"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["heightout"])) : 480};
+
 
     std::vector<std::pair<uint32_t, uint32_t> > hranges;
     for (auto a : stringtoolbox::split(commandlineArguments["hranges"], ',')) {
@@ -74,70 +87,94 @@ int32_t main(int32_t argc, char **argv) {
       std::cerr << argv[0] << ": Failed to access shared memory '" << SHMIN_NAME << "'." << std::endl;
     }
 
-    std::unique_ptr<cluon::SharedMemory> sharedMemoryOut(new cluon::SharedMemory{SHMOUT_NAME, WIDTH*HEIGHT*BPP/3});
+    std::unique_ptr<cluon::SharedMemory> sharedMemoryOut(new cluon::SharedMemory{SHMOUT_NAME, WIDTH*HEIGHT*BPP/8});
     if (sharedMemoryOut && sharedMemoryOut->valid()) {
       std::clog << argv[0] << ": Created shared memory for output image '" << sharedMemoryOut->name() << "' (" << sharedMemoryOut->size() << " bytes)." << std::endl;
     } else {
       std::cerr << argv[0] << ": Failed to create shared memory '" << SHMOUT_NAME << "'." << std::endl;
     }
 
-    std::fstream haarFile(HAARFILE.c_str(), std::ios::in);
-    if (!haarFile.good()) {
-      std::clog << argv[0] << ": Reading '" << HAARFILE << "' failed." << std::endl;
-    }
-
-    if ( (sharedMemoryIn && sharedMemoryIn->valid()) && (sharedMemoryOut && sharedMemoryOut->valid()) && haarFile.good() ) {
+    if ( (sharedMemoryIn && sharedMemoryIn->valid()) && (sharedMemoryOut && sharedMemoryOut->valid()) ) {
       if (VERBOSE) {
         std::clog << argv[0] << ": Starting image processing." << std::endl;
         std::clog << argv[0] << ": Cropping " << CROP_TOP << ", " << CROP_BOTTOM << std::endl;
-        std::clog << argv[0] << ": Processing the following H-color ranges: " << std::endl;
         for (auto b : hranges) {
         	std::clog << "  " << b.first << ".." << b.second << std::endl;
         }
       }
 
+      DetectKiwi kiwiDetector(HAARFILE);
+
       cluon::OD4Session od4{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
 
+      uint32_t const HEIGHT_SCALED_CROPPED = HEIGHT_SCALED - CROP_TOP - CROP_BOTTOM;
+
+      CvSize size;
+      size.width = WIDTH;
+      size.height = HEIGHT;
+
+      IplImage *image = cvCreateImageHeader(size, IPL_DEPTH_8U, BPP/8);
+      sharedMemoryIn->lock();
+      image->imageData = sharedMemoryIn->data();
+      image->imageDataOrigin = image->imageData;
+      sharedMemoryIn->unlock();
+
       while (od4.isRunning()) {
-        // Wait for a new image to come.
         sharedMemoryIn->wait();
 
+        cv::Mat processImage;
         {
-          // TODO: Do the image processing.
-          CvSize size;
-          size.width = WIDTH;
-          size.height = HEIGHT;
-
-          IplImage *image = cvCreateImageHeader(size, IPL_DEPTH_8U, BPP /*3 bytes per pixel in HSV color-space*/);
-
           sharedMemoryIn->lock();
-          {
-            image->imageData = sharedMemoryIn->data();
-            image->imageDataOrigin = image->imageData;
-          }
+          cv::Mat sourceImage = cv::cvarrToMat(image, false);
+          cv::resize(sourceImage, processImage, cv::Size(WIDTH_SCALED, HEIGHT_SCALED), 0, 0, cv::INTER_NEAREST);
           sharedMemoryIn->unlock();
-
-          cvReleaseImageHeader(&image);
         }
 
-        // TODO: Send results to OD4 session.
-        // od4.send(...);
+        processImage = processImage.rowRange(CROP_TOP, CROP_BOTTOM);
+
+        // Detect Kiwis.
+        auto detections = kiwiDetector.detect(processImage);
+        for (auto detection : detections) {
+          cv::Rect2d d = detection;
+          float angle = static_cast<float>(1.0 - (d.x + d.width / 2.0) / (WIDTH_SCALED / 2.0));
+          float distance = static_cast<float>((1.0 - (d.y + d.height) / HEIGHT_SCALED_CROPPED) / 0.2);
+
+          opendlv::logic::sensation::Point kiwiDetection;
+          kiwiDetection.azimuthAngle(angle);
+          kiwiDetection.distance(distance);
+
+          od4.send(kiwiDetection, cluon::time::now(), 0);
+        }
+
+        cv::Mat hsv;
+        cv::cvtColor(processImage, hsv, CV_BGR2HSV);
+
+        // Detect lanes.
+        cv::Mat lanes;
+        cv::inRange(hsv, cv::Scalar(20, 70, 170), cv::Scalar(60, 255, 255), lanes);
+
+        cv::Mat edges;
+        cv::Canny(lanes, edges, 50, 200, 3); 
+
+        std::vector<cv::Vec2f> detectedLines;
+        cv::HoughLines(lanes, detectedLines, 1, 5.0 * 3.14/180.0, 50.0);
+
+        (void) detectedLines;
+        // .. send in some format.
+
 
         // Share the results.
+        cv::Mat outImage;
+        cv::resize(processImage, outImage, cv::Size(WIDTH_OUT, HEIGHT_OUT), 0, 0, cv::INTER_NEAREST);
         {
           sharedMemoryOut->lock();
-          {
-            // TODO: Copy the data from image processing into shmout shared memory.
-          }
+          ::memcpy(sharedMemoryOut->data(), reinterpret_cast<char*>(outImage.data), outImage.step * outImage.rows);
           sharedMemoryOut->unlock();
-
-          // Notify any downstream sleeping processes for new data.
           sharedMemoryOut->notifyAll();
         }
       }
+      cvReleaseImageHeader(&image);
     }
-
-    haarFile.close();
   }
   return retCode;
 }
