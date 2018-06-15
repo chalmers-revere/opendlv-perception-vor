eb72893467566de4f49662df3f0001f90bcf9aee
olbender <olbender@users.noreply.github.com>
2018-06-15 06:22:15 +0200

    First complete version

--- src/opendlv-perception-vor.cpp
+++ src/opendlv-perception-vor.cpp
@@ -35,23 +35,23 @@
 int32_t main(int32_t argc, char **argv) {
   int32_t retCode{0};
   auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
-  if ( (0 == commandlineArguments.count("cid")) || (0 == commandlineArguments.count("width")) || (0 == commandlineArguments.count("height")) || (0 == commandlineArguments.count("bpp")) || (0 == commandlineArguments.count("haarfile")) || (0 == commandlineArguments.count("shmin")) || (0 == commandlineArguments.count("shmout")) || (0 == commandlineArguments.count("hranges")) ) {
+  if ( (0 == commandlineArguments.count("cid")) || (0 == commandlineArguments.count("width")) || (0 == commandlineArguments.count("height")) || (0 == commandlineArguments.count("bpp")) || (0 == commandlineArguments.count("haarfile")) ) {
     std::cerr << argv[0] << " accesses video data using shared memory and sends it as a stream over an OD4 session." << std::endl;
-    std::cerr << "         --verbose:    enable diagnostic output" << std::endl;
-    std::cerr << "         --croptop:    value for cropping image from top" << std::endl;
-    std::cerr << "         --cropbottom: value for cropping image from bottom" << std::endl;
-    std::cerr << "         --width:      the width of the image inside the shared memory" << std::endl;
-    std::cerr << "         --height:     the height of the image inside the shared memory" << std::endl;
-    std::cerr << "         --widthscaled: the width of the image used in image processing" << std::endl;
-    std::cerr << "         --heightscaled: the height of the image used in image processing" << std::endl;
-    std::cerr << "         --bpp:        the bits per pixel of the image inside the shared memory" << std::endl;
-    std::cerr << "         --haarfile:   name of the file containing the haar classifier" << std::endl;
-    std::cerr << "         --shmin:      name of the shared memory to read from (as consumer) the input image" << std::endl;
-    std::cerr << "         --shmout:     name of the shared memory to write to (as producer) the output image" << std::endl;
-    std::cerr << "         --widthout:   the width of the image inside the shared output memory" << std::endl;
-    std::cerr << "         --heightout:  the height of the image inside the shared output memory" << std::endl;
-    std::cerr << "         --hranges:    list of H-color ranges used for the image processing: min0:max0,min1:max1,min2:max2,..." << std::endl;
-    std::cerr << "Example: " << argv[0] << " --cid=111 --width=1280 --height=960 --bpp=24 --widthout=128 --heightout=96 --shmin=cam0 --shmout=output --hranges=1:2,3:4 --haarfile=myHaarFile.xml --croptop=10 --cropbottom=20 --verbose" << std::endl;
+    std::cerr << "         --verbose:        enable diagnostic output" << std::endl;
+    std::cerr << "         --croptop:        value for cropping image from top" << std::endl;
+    std::cerr << "         --croptophorizon: value for cropping image from top" << std::endl;
+    std::cerr << "         --cropbottom:     value for cropping image from bottom" << std::endl;
+    std::cerr << "         --width:          the width of the image inside the shared memory" << std::endl;
+    std::cerr << "         --height:         the height of the image inside the shared memory" << std::endl;
+    std::cerr << "         --widthscaled:    the width of the image used in image processing" << std::endl;
+    std::cerr << "         --heightscaled:   the height of the image used in image processing" << std::endl;
+    std::cerr << "         --bpp:            the bits per pixel of the image inside the shared memory" << std::endl;
+    std::cerr << "         --haarfile:       name of the file containing the haar classifier" << std::endl;
+    std::cerr << "         --shmin:          name of the shared memory to read from (as consumer) the input image" << std::endl;
+    std::cerr << "         --shmout:         name of the shared memory to write to (as producer) the output image" << std::endl;
+    std::cerr << "         --widthout:       the width of the image inside the shared output memory" << std::endl;
+    std::cerr << "         --heightout:      the height of the image inside the shared output memory" << std::endl;
+    std::cerr << "Example: " << argv[0] << " --cid=111 --width=1280 --height=960 --bpp=24 --widthscaled=256 --heightscaled=192 --widthout=128 --heightout=96 --shmin=cam0 --shmout=output --haarfile=myHaarFile.xml --croptop=10 --cropbottom=20 --croptophorizon=50 --verbose" << std::endl;
     retCode = 1;
   } else {
     bool const VERBOSE{commandlineArguments.count("verbose") != 0};
@@ -59,26 +59,20 @@ int32_t main(int32_t argc, char **argv) {
     uint32_t const WIDTH{static_cast<uint32_t>(std::stoi(commandlineArguments["width"]))};
     uint32_t const HEIGHT{static_cast<uint32_t>(std::stoi(commandlineArguments["height"]))};
     uint32_t const BPP{static_cast<uint32_t>(std::stoi(commandlineArguments["bpp"]))};
-    uint32_t const CROP_TOP{static_cast<uint32_t>(std::stoi(commandlineArguments["croptop"]))};
-    uint32_t const CROP_BOTTOM{static_cast<uint32_t>(std::stoi(commandlineArguments["cropbottom"]))};
     std::string const HAARFILE{commandlineArguments["haarfile"]};
+
     std::string const SHMIN_NAME{(commandlineArguments["shmin"].size() > 0) ? commandlineArguments["shmin"] : "/cam0"};
     std::string const SHMOUT_NAME{(commandlineArguments["shmout"].size() > 0) ? commandlineArguments["shmout"] : "/output"};
-
-    uint32_t const WIDTH_SCALED{(commandlineArguments["widthscaled"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["widthscaled"])) : 128};
-    uint32_t const HEIGHT_SCALED{(commandlineArguments["heightscaled"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["heightscaled"])) : 96};
     
-    uint32_t const WIDTH_OUT{(commandlineArguments["widthout"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["widthout"])) : 640};
-    uint32_t const HEIGHT_OUT{(commandlineArguments["heightout"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["heightout"])) : 480};
+    uint32_t const CROP_TOP{(commandlineArguments["croptop"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["croptop"])) : 0};
+    uint32_t const CROP_BOTTOM{(commandlineArguments["cropbottom"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["cropbottom"])) : 0};
+    uint32_t const CROP_TOP_HORIZON{(commandlineArguments["croptophorizon"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["croptophorizion"])) : 0};
 
-
-    std::vector<std::pair<uint32_t, uint32_t> > hranges;
-    for (auto a : stringtoolbox::split(commandlineArguments["hranges"], ',')) {
-        auto b = stringtoolbox::split(a, ':');
-        if (2 == b.size()) {
-            hranges.push_back(std::make_pair(std::stoi(b.at(0)), std::stoi(b.at(1))));
-        }
-    }
+    uint32_t const WIDTH_SCALED{(commandlineArguments["widthscaled"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["widthscaled"])) : WIDTH};
+    uint32_t const HEIGHT_SCALED{(commandlineArguments["heightscaled"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["heightscaled"])) : HEIGHT};
+    
+    uint32_t const WIDTH_OUT{(commandlineArguments["widthout"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["widthout"])) : WIDTH};
+    uint32_t const HEIGHT_OUT{(commandlineArguments["heightout"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["heightout"])) : HEIGHT};
 
     std::unique_ptr<cluon::SharedMemory> sharedMemoryIn(new cluon::SharedMemory{SHMIN_NAME});
     if (sharedMemoryIn && sharedMemoryIn->valid()) {
@@ -97,11 +91,12 @@ int32_t main(int32_t argc, char **argv) {
     if ( (sharedMemoryIn && sharedMemoryIn->valid()) && (sharedMemoryOut && sharedMemoryOut->valid()) ) {
       if (VERBOSE) {
         std::clog << argv[0] << ": Starting image processing." << std::endl;
-        std::clog << argv[0] << ": Cropping " << CROP_TOP << ", " << CROP_BOTTOM << std::endl;
-        for (auto b : hranges) {
-        	std::clog << "  " << b.first << ".." << b.second << std::endl;
-        }
       }
+
+      uint32_t const KIWI_ID = 0;
+      uint32_t const BLUE_BOX_ID = 1;
+      uint32_t const PURPLE_BOX_ID = 2;
+      uint32_t const LANE_MARKING_ID = 3;
 
       DetectKiwi kiwiDetector(HAARFILE);
 
@@ -122,36 +117,138 @@ int32_t main(int32_t argc, char **argv) {
       while (od4.isRunning()) {
         sharedMemoryIn->wait();
 
-        cv::Mat processImage;
+        cv::Mat originalScaledImage;
         {
           sharedMemoryIn->lock();
           cv::Mat sourceImage = cv::cvarrToMat(image, false);
-          cv::resize(sourceImage, processImage, cv::Size(WIDTH_SCALED, HEIGHT_SCALED), 0, 0, cv::INTER_NEAREST);
+          cv::resize(sourceImage, originalScaledImage, cv::Size(WIDTH_SCALED, HEIGHT_SCALED), 0, 0, cv::INTER_NEAREST);
           sharedMemoryIn->unlock();
         }
 
-        processImage = processImage.rowRange(CROP_TOP, CROP_BOTTOM);
+        cv::Mat objectImage = originalScaledImage.rowRange(CROP_TOP, CROP_BOTTOM);
+        cv::Mat lineImage = originalScaledImage.rowRange(CROP_TOP_HORIZON, CROP_BOTTOM);
+
+        // Note: originalScaledImage will here after be drawn at.
 
         // Detect Kiwis.
-        auto detections = kiwiDetector.detect(processImage);
+        auto detections = kiwiDetector.detect(objectImage);
         for (auto detection : detections) {
           cv::Rect2d d = detection;
           float angle = static_cast<float>(1.0 - (d.x + d.width / 2.0) / (WIDTH_SCALED / 2.0));
           float distance = static_cast<float>((1.0 - (d.y + d.height) / HEIGHT_SCALED_CROPPED) / 0.2);
 
+          cv::rectangle(originalScaledImage, cv::Point(detection.tl().x, detection.tl().y + CROP_TOP), cv::Point(detection.br().x, detection.br().y + CROP_TOP), cv::Scalar(0, 0, 150), 3, 8, 0);
+
           opendlv::logic::sensation::Point kiwiDetection;
           kiwiDetection.azimuthAngle(angle);
           kiwiDetection.distance(distance);
+          od4.send(kiwiDetection, cluon::time::now(), KIWI_ID);
 
-          od4.send(kiwiDetection, cluon::time::now(), 0);
+          if (VERBOSE) {
+            std::clog << argv[0] << ": Detected Kiwi at angle " << angle << " and distance " << distance << std::endl;
+          }
         }
 
         cv::Mat hsv;
-        cv::cvtColor(processImage, hsv, CV_BGR2HSV);
+        cv::cvtColor(objectImage, hsv, CV_BGR2HSV);
+
+        int erosionSize = 2;
+        cv::Mat element = getStructuringElement(cv::MORPH_RECT, 
+            cv::Size(2 * erosionSize + 1, 2 * erosionSize + 1), 
+            cv::Point(erosionSize, erosionSize));
+
+        // Detect blue boxes.
+        {
+          cv::Mat blueBoxes;
+          cv::inRange(hsv, cv::Scalar(85, 140, 140), cv::Scalar(125, 255, 255), blueBoxes);
+
+          cv::Mat blueBoxMask;
+          cv::erode(blueBoxes, blueBoxMask, element);
+          cv::dilate(blueBoxMask, blueBoxMask, element);
+
+          std::vector<std::vector<cv::Point>> contoursBlue;
+          std::vector<cv::Vec4i> hierarchyBlue;
+
+          findContours(blueBoxMask, contoursBlue, hierarchyBlue, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+          
+          double maxSize = 0;
+          int32_t iLabel = 0;
+          if (contoursBlue.size() != 0) {
+            for (uint32_t i = 0; i < contoursBlue.size(); i++) {
+              double area = contourArea(contoursBlue[i], false);
+
+              if (area > maxSize) {
+                maxSize = area;
+                iLabel = i;
+              }
+            }
+          
+            cv::Rect r = boundingRect(contoursBlue[iLabel]);
+            cv::rectangle(originalScaledImage, cv::Point(r.tl().x, r.tl().y + CROP_TOP), cv::Point(r.br().x, r.br().y + CROP_TOP), cv::Scalar(150, 0, 0), 3, 8, 0);
+
+            float angle = static_cast<float>(1.0 - (r.x + r.width / 2.0) / (WIDTH_SCALED / 2.0));
+            float distance = static_cast<float>((1.0 - (r.y + r.height) / HEIGHT_SCALED_CROPPED) / 0.2);
+
+            opendlv::logic::sensation::Point blueDetection;
+            blueDetection.azimuthAngle(angle);
+            blueDetection.distance(distance);
+            od4.send(blueDetection, cluon::time::now(), BLUE_BOX_ID);
+
+            if (VERBOSE) {
+              std::clog << argv[0] << ": Detected blue box at angle " << angle << " and distance " << distance << std::endl;
+            }
+          }
+        }
+
+        // Detect purple boxes.
+        {
+          cv::Mat purpleBoxes;
+          cv::inRange(hsv, cv::Scalar(115, 40, 160), cv::Scalar(155, 255, 255), purpleBoxes);
+
+          cv::Mat purpleBoxMask;
+          cv::erode(purpleBoxes, purpleBoxMask, element);
+          cv::dilate(purpleBoxMask, purpleBoxMask, element);
+
+          std::vector<std::vector<cv::Point>> contoursPurple;
+          std::vector<cv::Vec4i> hierarchyPurple;
+          
+          findContours(purpleBoxMask, contoursPurple, hierarchyPurple, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+          
+          double maxSize = 0;
+          int32_t iLabel = 0;
+          if (contoursPurple.size() != 0) {
+            for (uint32_t i = 0; i < contoursPurple.size(); i++) {
+              double area = contourArea(contoursPurple[i], false);
+
+              if (area > maxSize) {
+                maxSize = area;
+                iLabel = i;
+              }
+            }
+          
+            cv::Rect r = boundingRect(contoursPurple[iLabel]);
+            cv::rectangle(originalScaledImage, cv::Point(r.tl().x, r.tl().y + CROP_TOP), cv::Point(r.br().x, r.br().y + CROP_TOP), cv::Scalar(100, 0, 150), 3, 8, 0);
+
+            float angle = static_cast<float>(1.0 - (r.x + r.width / 2.0) / (WIDTH_SCALED / 2.0));
+            float distance = static_cast<float>((1.0 - (r.y + r.height) / HEIGHT_SCALED_CROPPED) / 0.2);
+
+            opendlv::logic::sensation::Point purpleDetection;
+            purpleDetection.azimuthAngle(angle);
+            purpleDetection.distance(distance);
+            od4.send(purpleDetection, cluon::time::now(), PURPLE_BOX_ID);
+
+            if (VERBOSE) {
+              std::clog << argv[0] << ": Detected purple box at angle " << angle << " and distance " << distance << std::endl;
+            }
+          }
+        }
 
         // Detect lanes.
+        cv::Mat hsvLines;
+        cv::cvtColor(lineImage, hsvLines, CV_BGR2HSV);
+        
         cv::Mat lanes;
-        cv::inRange(hsv, cv::Scalar(20, 70, 170), cv::Scalar(60, 255, 255), lanes);
+        cv::inRange(hsvLines, cv::Scalar(20, 70, 170), cv::Scalar(60, 255, 255), lanes);
 
         cv::Mat edges;
         cv::Canny(lanes, edges, 50, 200, 3); 
@@ -159,13 +256,34 @@ int32_t main(int32_t argc, char **argv) {
         std::vector<cv::Vec2f> detectedLines;
         cv::HoughLines(lanes, detectedLines, 1, 5.0 * 3.14/180.0, 50.0);
 
-        (void) detectedLines;
-        // .. send in some format.
+        for (auto v : detectedLines) {
+          float rho = v[0];
+          float theta = v[1];
+          cv::Point pt1;
+          cv::Point pt2;
+          double a = cos(theta);
+          double b = sin(theta);
+          double x0 = a * rho;
+          double y0 = b * rho;
+          pt1.x = cvRound(x0 + 1000*(-b));
+          pt1.y = cvRound(y0 + 1000*(a)) + CROP_TOP_HORIZON;
+          pt2.x = cvRound(x0 - 1000*(-b));
+          pt2.y = cvRound(y0 - 1000*(a)) + CROP_TOP_HORIZON;
+          cv::line(originalScaledImage, pt1, pt2, cv::Scalar(0, 150, 150), 3, CV_AA);
 
+          opendlv::logic::sensation::Point lineDetection;
+          lineDetection.azimuthAngle(theta);
+          lineDetection.distance(rho);
+          od4.send(lineDetection, cluon::time::now(), LANE_MARKING_ID);
+            
+          if (VERBOSE) {
+            std::clog << argv[0] << ": Detected lane marking at polar coordinates with angle " << theta << " and distance " << rho << std::endl;
+          }
+        }
 
         // Share the results.
         cv::Mat outImage;
-        cv::resize(processImage, outImage, cv::Size(WIDTH_OUT, HEIGHT_OUT), 0, 0, cv::INTER_NEAREST);
+        cv::resize(originalScaledImage, outImage, cv::Size(WIDTH_OUT, HEIGHT_OUT), 0, 0, cv::INTER_NEAREST);
         {
           sharedMemoryOut->lock();
           ::memcpy(sharedMemoryOut->data(), reinterpret_cast<char*>(outImage.data), outImage.step * outImage.rows);
